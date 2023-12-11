9188a5e9d7cdf285e8ca32b9bb1eec28ce9df5f7
Mose Schmiedel <mose@schmiednet.de>
2023-12-11 22:01:27 +0100

    day5: use `Range Integer Integer` instead of direct Integers

--- day5/day5.hs
+++ day5/day5.hs
@@ -3,13 +3,23 @@ import Parser (Parser, parse, int, char, string, many, space, chomp, newline)
 import Data.Maybe (fromJust, mapMaybe, fromMaybe, catMaybes)
 import Data.Function ((&))
 import Data.Functor ((<&>))
-import Data.List (find)
+import Data.List (find, nub, singleton)
 import Control.Parallel (par)
 import Control.Parallel.Strategies (parMap, rdeepseq, parListChunk, using)
 import Control.Monad (ap, Functor, (>=>))
+import Data.Traversable (sequence)
 
 -- MapRange source dest length  
 data MapRange = MapRange Integer Integer Integer deriving (Show, Eq)
+
+-- Range start end (inclusive)
+data Range = Range Integer Integer deriving (Show, Eq)
+
+mkRange :: Integer -> Integer -> Maybe Range
+mkRange s e = if s <= e then Just $ Range s e else Nothing
+
+intersect :: Range -> Range -> Maybe Range
+intersect (Range sa ea) (Range sb eb) = mkRange (max sa sb) (min ea eb)
 
 inMR :: Integer -> MapRange -> Bool
 inMR k (MapRange source dest len) = source <= k && k < source + len
@@ -31,43 +41,41 @@ rIntMR range (MapRange source dest len) =
 (%>) :: MapRange -> Integer -> Integer
 (%>) (MapRange source dest _) k = dest + (k - source)
 
-(%%>) :: MapRange -> Range -> [Range]
+(%%>) :: MapRange -> Range -> (Maybe Range, [Range])
 (%%>) (MapRange source dest len) (Range s e) =
   maybe
-  [destToRange mr]
+  (Nothing, [Range s e])
   (\(MapRange is id ilen) ->
-    catMaybes [ mkRange s (is - 1)
-              , Just (destToRange $ MapRange is id ilen)
-              , mkRange (is + ilen) e
-              ])
-  (rIntMR r mr)
-  where
-    mr = MapRange source dest len
-    r = Range s e
+     ( Just (destToRange $ MapRange is id ilen)
+     , catMaybes [ mkRange s (is - 1)
+                 , mkRange (is + ilen) e
+                 ]
+     ))
+  (rIntMR (Range s e) (MapRange source dest len))
 
+runRec :: [Range] -> [Range] -> [MapRange] -> [Range]
+runRec [] resolved _ = resolved
+runRec rgs resolved [] = nub $ resolved ++ rgs
+runRec rgs resolved (mr:mrs) =
+  let
+    transformed = map (mr %%>) rgs
+    resolved' = mapMaybe fst transformed ++ resolved
+    rest = nub $ concatMap snd transformed
+  in runRec rest resolved' mrs
 
--- Range start length (inclusive)
-data Range = Range Integer Integer deriving (Show, Eq)
-
-mkRange :: Integer -> Integer -> Maybe Range
-mkRange s e = if s <= e then Just $ Range s e else Nothing
-
-intersect :: Range -> Range -> Maybe Range
-intersect (Range sa ea) (Range sb eb) = mkRange (max sa sb) (min ea eb)
+run :: [MapRange] -> [Range] -> [Range]
+run mrs rgs = runRec rgs [] mrs
 
 minR :: Range -> Range -> Range
-minR (Range sa ea) (Range sb eb) =
-  if sa < sb
-  then Range sa ea
-  else if sb < sa
-  then Range sb eb
-  else if ea < eb
-  then Range sa ea
-  else Range sb eb
+minR (Range sa ea) (Range sb eb)
+  | sa < sb = Range sa ea
+  | sb < sa = Range sb eb
+  | ea < eb = Range sa ea
+  | otherwise = Range sb eb
 
-minimumR :: [Range] -> Range
-minimumR = foldl minR
-
+minimumR :: [Range] -> Maybe Range
+minimumR [] = Nothing
+minimumR (rg:rgs) = Just $ foldl minR rg rgs
 
 seedParser :: Parser () [Int]
 seedParser = do
@@ -92,49 +100,41 @@ maprange = do
   space
   source <- int
   space
-  len <- int
-  return $ MapRange (toInteger source) (toInteger dest) (toInteger len)
+  int <&> MapRange (toInteger source) (toInteger dest) . toInteger
 
-
-mapParser :: Eq e => String -> Parser e (Integer -> Integer)
+mapParser :: Eq e => String -> Parser e [MapRange]
 mapParser identifier = do
   chomp $ string identifier
   space
   newline
-  mrs <- many (maprange <* newline)
-  return $ \k ->
-    maybe k (%> k) (find (inMR k) mrs)
+  many (maprange <* newline)
 
-seedSoilParser :: Parser () (Integer -> Integer)
+seedSoilParser :: Parser () [MapRange]
 seedSoilParser = mapParser "seed-to-soil map:"
 
-soilFertParser :: Parser () (Integer -> Integer)
+soilFertParser :: Parser () [MapRange]
 soilFertParser = mapParser "soil-to-fertilizer map:"
 
-fertWaterParser :: Parser () (Integer -> Integer)
+fertWaterParser :: Parser () [MapRange]
 fertWaterParser = mapParser "fertilizer-to-water map:"
 
-waterLightParser :: Parser () (Integer -> Integer)
+waterLightParser :: Parser () [MapRange]
 waterLightParser = mapParser "water-to-light map:"
 
-lightTempParser :: Parser () (Integer -> Integer)
+lightTempParser :: Parser () [MapRange]
 lightTempParser = mapParser "light-to-temperature map:"
 
-tempHumidParser :: Parser () (Integer -> Integer)
+tempHumidParser :: Parser () [MapRange]
 tempHumidParser = mapParser "temperature-to-humidity map:"
 
-humidLocParser :: Parser () (Integer -> Integer)
+humidLocParser :: Parser () [MapRange]
 humidLocParser = mapParser "humidity-to-location map:"
-
-resolveMapping :: (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer) -> (Integer -> Integer) -> Int -> Integer
-resolveMapping seedSoil soilFert fertWater waterLight lightTemp tempHumid humidLoc =
-  humidLoc . tempHumid . lightTemp . waterLight . fertWater . soilFert . seedSoil . toInteger
 
 main :: IO ()
 main  = do
   input <- readFile "day5/input.txt"
   print $ do
-    seeds <- parse seedParser input
+    seeds <- parse (seedParser <&> map ((\int -> Range int int) . toInteger)) input
     seedSoil <- parse seedSoilParser input
     soilFert <- parse soilFertParser input
     fertWater <- parse fertWaterParser input
@@ -142,8 +142,8 @@ main  = do
     lightTemp <- parse lightTempParser input
     tempHumid <- parse tempHumidParser input
     humidLoc <- parse humidLocParser input
-    let locs =  map (resolveMapping seedSoil soilFert fertWater waterLight lightTemp tempHumid humidLoc) seeds
-    return $ minimum locs
+    let locs =  (run humidLoc . run tempHumid . run lightTemp . run waterLight . run fertWater . run soilFert . run seedSoil) seeds
+    return $ minimumR locs
   print $ do
     seeds <- parse seedRangeParser input
     seedSoil <- parse seedSoilParser input
@@ -153,6 +153,6 @@ main  = do
     lightTemp <- parse lightTempParser input
     tempHumid <- parse tempHumidParser input
     humidLoc <- parse humidLocParser input
-    let locs =  map (\sr -> seedSoil %%> sr >=> (%%>) soilFert >=> (%%>) fertWater >=> (%%>) waterLight >=> (%%>) lightTemp >=> (%%>) tempHumid >=> (%%>) humidLoc) seeds
+    let locs =  (run humidLoc . run tempHumid . run lightTemp . run waterLight . run fertWater . run soilFert . run seedSoil) seeds
     return $ minimumR locs
 
